b7fb24e375330faac83bf0c0e724b8cb20539c7a
pratik <pratikgaikwad551@gmail.com>
2020-09-25 23:58:06 +0530

    Create Wheel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+   <div className="arrow"></div>
+      
+      <ul className="circle">
+        <li>
+          <div className="text">₹50 Cash</div>
+        </li>
+        <li>
+          <div className="text">Extra Spin</div>
+        </li>
+        <li>
+          <div className="text">2X Savings</div>
+        </li>
+        <li>
+          <div className="text">₹100 Cashback</div>
+        </li>
+        <li>
+          <div className="text">₹20 Cash</div>
+        </li>
+        <li>
+          <div className="text">₹1000 Cash</div>
+        </li>
+        <li>
+          <div className="text">1.5X Savings</div>
+        </li>
+        <li>
+          <div className="text">2X Savings</div>
+        </li>
+      </ul>
+      <button className="spin-button">SPIN</button>
+      <h1>Spin the Wheel now to get the Rewards</h1>
+   </div>
   );
 }
+
 
 export default App;
