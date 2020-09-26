3fac748673976e3b5e06a9835d3e110e24f8a0de
pratik <pratikgaikwad551@gmail.com>
2020-09-26 09:43:58 +0530

    Updates

---
+++ precache-manifest.1171a1dab32ded3e96a6367b6a52c1a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4fd93460885ef8cf0b0757e2b0c3f6",
+    "url": "/spin-wheel/index.html"
+  },
+  {
+    "revision": "7d026dc688fccb53e2e8",
+    "url": "/spin-wheel/static/css/main.bde54a3d.chunk.css"
+  },
+  {
+    "revision": "88de4d92bb52374c77e6",
+    "url": "/spin-wheel/static/js/2.3dcb103d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spin-wheel/static/js/2.3dcb103d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d026dc688fccb53e2e8",
+    "url": "/spin-wheel/static/js/main.3b422e79.chunk.js"
+  },
+  {
+    "revision": "afdc583e19c19f34ff6e",
+    "url": "/spin-wheel/static/js/runtime-main.7c28bc5c.js"
+  }
+]);
